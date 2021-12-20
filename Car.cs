25cdab4f5328d14cd10d85784e40d5c8e6eb50f3
GurgenOhanyan <78640070+GurgenOhanyan@users.noreply.github.com>
2021-12-20 18:33:44 +0400

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functional_Programming
+{
+    class Car
+    {
+        public delegate void CarEvent(string message);
+
+        private int speed;
+
+        private readonly int maxSpeed;
+
+        private CarEvent carEventHandler;
+
+        public Car(int maxSpeed)
+        {
+            this.maxSpeed = maxSpeed;
+        }
+
+        public int Speed => speed;
+
+        public void RegisterCarEventHandler(CarEvent handler)
+        {
+            this.carEventHandler += handler;
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (this.speed == 0 && delta > 0)
+                if (this.carEventHandler != null)
+                    this.carEventHandler("INFO: Car started to move.");
+
+            this.speed += delta;
+
+            if (this.speed >= this.maxSpeed)
+            {
+                this.carEventHandler?.Invoke("WARNING: Max speed is reached");
+                this.speed = this.maxSpeed;
+            }
+
+            if (this.speed == 0)
+                this.carEventHandler?.Invoke("INFO: Car is stopped");
+        }
+
+        public void DeregisterCarEventHandler(CarEvent handler)
+        {
+            this.carEventHandler -= handler;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Functional_Programming
+{
+    class Program
+    {
+        public delegate void PrintDelegate(string message);
+
+        private static void PrintMethod(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void Main(string[] args)
+        {
+
+            Car car = new Car(60);
+
+            car.RegisterCarEventHandler(OnCarEvent);
+            //Registering Second method referance  
+            car.RegisterCarEventHandler(AnotherCarEvent);
+            for (int i = 0; i < 3; i++)
+            {
+                car.Accelerate(20);
+                Console.WriteLine($"Car Speed -- {car.Speed}");
+            }
+            Console.WriteLine("--------------------------------------------");
+            for (int i = 0; i < 3; i++)
+            {
+                car.Accelerate(-20);
+                Console.WriteLine($"Car Speed -- {car.Speed}");
+            }
+            // ===========================
+
+            //Func<int, int, int> mathAdd = (int x, int y) => x + y;
+            //Func<int, int> mathAbs =
+            //    (int x) =>
+            //    {
+            //        if (x < 0)
+            //            return -x;
+            //        else
+            //            return x;
+            //    };
+
+            List<int> numbers = new List<int> { 42, 17, 65, 24, 31, 6, 58, 0 };
+            Predicate<int> isEvenPredicate = new Predicate<int>(Program.IsEvenNumber);
+            List<int> evenNumbers = numbers.FindAll(isEvenPredicate);
+            foreach (int number in evenNumbers)
+                Console.Write($"{number}, ");
+            Console.WriteLine();
+
+
+            //Lambda instead of delegate
+            List<int> evenNumbersWithLambda = numbers.FindAll(n => n % 2 == 0);
+
+        }
+        private static bool IsEvenNumber(int x)
+        {
+            return x % 2 == 0;
+        }
+        static void OnCarEvent(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        static void AnotherCarEvent(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
